2e8b0e44a3eefc1e4bdef72f87d3aa9cee42992f
zoegerolemou <2023949g@student.gla.ac.uk>
2016-02-20 22:18:44 +0000

    Completed Chapter 8 - Fun with Forms

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -21,9 +22,51 @@ def category(request, category_name_slug):
 		pages = Page.objects.filter(category=category)
 		context_dict['pages'] = pages
 		context_dict['category'] = category
+		context_dict['category_name_slug'] = category_name_slug
 	except Category.DoesNotExist:
 		pass
 
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print form.errors
+	
+	else:
+		form = CategoryForm()
+
+	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+	try:
+		cat = Category.objects.get(slug = category_name_slug)
+	except:
+		cat = None
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+				return category(request, category_name_slug)
+		else:
+			print form.errors
+	else:
+		form = PageForm()
+
+	context_dict = {'form': form, 'category': cat}
+
+	return render(request, 'rango/add_page.html', context_dict)
+
+
 
 
