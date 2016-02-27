b11c501a5309a5a299edfab649496661eb7c99bd
zoegerolemou <2023949g@student.gla.ac.uk>
2016-02-27 17:40:24 +0000

    Completed Chapter 11 - Cookies and Sessions

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,16 +4,48 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
 	page_list = Page.objects.order_by('-views')[:5]	
 	context_dict = {'categories': category_list, 'pages': page_list}
-	return render(request, 'rango/index.html', context_dict)
+	visits = request.session.get('visits')
+	
+	if not visits:
+		visits = 1
+
+	reset_last_visit_time = False
+	last_visit = request.session.get('last_visit')
+	
+	if last_visit:
+		last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+		if (datetime.now() - last_visit_time).days > 0:
+			visits = visits + 1
+			reset_last_visit_time = True
+
+	else:
+		reset_last_visit_time = True
+	
+	if reset_last_visit_time:
+		request.session['last_visit'] = str(datetime.now())
+		request.session['visits'] = visits
+
+	context_dict['visits'] = visits	
+	response = render(request, 'rango/index.html', context_dict)
+
+	return response
 
 def about(request):
 	context_dict = {'boldmessage': "I am bold font from the context"}
-	return render(request, 'rango/about.html', context_dict)
+
+	if request.session.get('visits'):
+		count = request.session.get('visits')
+	else:
+		count = 0
+
+	return render(request, 'rango/about.html', {'visits': count})
 
 def category(request, category_name_slug):
 	context_dict = {}
@@ -58,7 +90,7 @@ def add_page(request, category_name_slug):
 		form = PageForm(request.POST)
 		if form.is_valid():
 			if cat:
-				page = form.save(commit=False)
+				page = form.save(commit = False)
 				page.category = cat
 				page.views = 0
 				page.save()
